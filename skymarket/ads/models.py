2622ffc52d608afe2969ce5772e6572e693bc478
S.P <sergey.a.protsenko@mail.ru>
2022-09-23 14:47:23 +0700

    ads and commets models are created

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,12 +1,30 @@
-from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    title = models.CharField(max_length=200, null=False)
+    price = models.PositiveIntegerField(default=0)
+    description = models.CharField(max_length=1000, blank=True, null=False)
+    author = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    text = models.CharField(max_length=1000, null=False)
+    author = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE, null=True)
+    ad = models.ForeignKey(to=Ad, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
+        ordering = ['-created_at']
