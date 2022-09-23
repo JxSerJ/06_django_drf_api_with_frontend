24b2754182c7d7f775939b47b0abd50203168c17
S.P <sergey.a.protsenko@mail.ru>
2022-09-23 13:15:05 +0700

    urls for authtntication and users views registered

--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -7,6 +7,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path("api/admin/", admin.site.urls),
     path("api/redoc-tasks/", include("redoc.urls")),
+    path("api/", include("users.urls")),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'))
--- skymarket/users/urls.py
+++ skymarket/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from djoser.views import UserViewSet
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 users_router = SimpleRouter()
 
@@ -8,4 +9,6 @@ users_router.register(prefix='users', viewset=UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(users_router.urls)),
+    path('token/', TokenObtainPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
 ]
